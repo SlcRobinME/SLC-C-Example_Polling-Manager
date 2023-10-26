8f069187ac5c13bb47f563b8a104774f06dda9a1
EdibSU <edib.supic@skyline.be>
2023-10-26 22:49:15 +0200

    Multiple classes polling.
    Change-Id: Ie67e46db77143e9a760500ffac61192e6bcca1c1

---
+++ QAction_1/Client/Pollables.cs
@@ -0,0 +1,74 @@
+﻿namespace Skyline.PollingManager.Client
+{
+    using Skyline.DataMiner.Scripting;
+
+    using Skyline.PollingManager.Enums;
+    using Skyline.PollingManager.Pollable;
+
+    public class PollableA : PollableBase
+	{
+		public PollableA(SLProtocol protocol, string name) : base(protocol, name)
+		{
+			PeriodType = PeriodType.Custom;
+		}
+
+		public PollableA(SLProtocol protocol, object[] row) : base(protocol, row)
+		{
+		}
+
+		public override bool Poll()
+		{
+			Protocol.Log($"$$$$$ Logging from [{nameof(PollableA)}] the group [{Name}] $$$$$");
+
+			return true;
+		}
+	}
+
+    public class PollableB : PollableBase
+	{
+		public PollableB(SLProtocol protocol, string name, double? period = null) : base(protocol, name)
+		{
+			if (period != null)
+			{
+				Period = period.Value;
+				PeriodType = PeriodType.Custom;
+			}
+		}
+
+		public PollableB(SLProtocol protocol, object[] row) : base(protocol, row)
+		{
+		}
+
+		public override bool Poll()
+		{
+			Protocol.Log($"##### Logging from [{nameof(PollableB)}] the group [{Name}] #####");
+
+			return true;
+		}
+	}
+
+    public class PollableC : PollableBase
+	{
+		public PollableC(SLProtocol protocol, string name) : base(protocol, name)
+		{
+		}
+
+		public PollableC(SLProtocol protocol, string name, double period, double defaultPeriod, PeriodType periodType) : base(protocol, name)
+		{
+			Period = period;
+			DefaultPeriod = defaultPeriod;
+			PeriodType = periodType;
+		}
+
+		public PollableC(SLProtocol protocol, object[] row) : base(protocol, row)
+		{
+		}
+
+		public override bool Poll()
+		{
+			Protocol.Log($"@@@@@ Logging from [{nameof(PollableC)}] the group [{Name}] @@@@@");
+
+			return true;
+		}
+	}
+}
--- QAction_1/Client/PollingManagerConfiguration.cs
+++ QAction_1/Client/PollingManagerConfiguration.cs
@@ -1,55 +1,56 @@
 ﻿namespace Skyline.PollingManager.Client
 {
-	using System.Collections.Generic;
-	using System.Linq;
+    using System.Collections.Generic;
+    using System.Linq;
 
-	using Skyline.PollingManager.Interfaces;
-	using Skyline.PollingManager.Providers;
-	using Skyline.PollingManager.Structs;
+    using Skyline.PollingManager.Enums;
+    using Skyline.PollingManager.Pollable;
+    using Skyline.PollingManager.Providers;
+    using Skyline.PollingManager.Structs;
 
-	public static class PollingManagerConfiguration
+    public static class PollingManagerConfiguration
 	{
 		private static readonly Dictionary<string, PollableBase> _rows = new Dictionary<string, PollableBase>()
 		{
 			// Parent of CEO, CFO, CTO
 			// Child of -
-			{ "Owner", new Pollable(SLProtocolProvider.Protocol, "Owner") },
+			{ "Owner", new PollableA(SLProtocolProvider.Protocol, "Owner - A") },
 
 			// Parent of CFO, CTO, Expert Hub Lead
 			// Child of Owner
-			{ "CEO", new Pollable(SLProtocolProvider.Protocol, "CEO") },
+			{ "CEO", new PollableA(SLProtocolProvider.Protocol, "CEO - A") },
 
 			// Parent of -
 			// Child of Owner, CEO
-			{ "CFO", new Pollable(SLProtocolProvider.Protocol, "CFO") },
+			{ "CFO", new PollableB(SLProtocolProvider.Protocol, "CFO - B", 15) },
 
 			// Parent of Expert Hub Lead
 			// Child of Owner, CEO
-			{ "CTO", new Pollable(SLProtocolProvider.Protocol, "CTO") },
+			{ "CTO", new PollableB(SLProtocolProvider.Protocol, "CTO - B", 20) },
 
 			// Parent of Principal 1, Principal 2, Senior 1
 			// Child of CEO, CTO
-			{ "Expert Hub Lead", new Pollable(SLProtocolProvider.Protocol, "Expert Hub Lead") },
+			{ "Expert Hub Lead", new PollableB(SLProtocolProvider.Protocol, "Expert Hub Lead - B") },
 
 			// Parent of Senior 2, Senior 3
 			// Child of CTO, Expert Hub Lead
-			{ "Principal 1", new Pollable(SLProtocolProvider.Protocol, "Principal 1") },
+			{ "Principal 1", new PollableB(SLProtocolProvider.Protocol, "Principal 1 - B") },
 
 			// Parent of Senior 1
 			// Child of CTO, Expert Hub Lead
-			{ "Principal 2", new Pollable(SLProtocolProvider.Protocol, "Principal 2") },
+			{ "Principal 2", new PollableC(SLProtocolProvider.Protocol, "Principal 2 - C", 30, 60, PeriodType.Default) },
 
 			// Parent of -
 			// Child of Expert Hub Lead, Principal 2
-			{ "Senior 1", new Pollable(SLProtocolProvider.Protocol, "Senior 1") },
+			{ "Senior 1", new PollableA(SLProtocolProvider.Protocol, "Senior 1 - A") },
 
 			// Parent of -
 			// Child of Principal 1
-			{ "Senior 2", new Pollable(SLProtocolProvider.Protocol, "Senior 2") },
+			{ "Senior 2", new PollableB(SLProtocolProvider.Protocol, "Senior 2 - B") },
 
 			// Parent of -
 			// Child of Principal 1
-			{ "Senior 3", new Pollable(SLProtocolProvider.Protocol, "Senior 3") },
+			{ "Senior 3", new PollableC(SLProtocolProvider.Protocol, "Senior 3 - C") },
 		};
 
 		private static readonly List<Dependency> _dependencies = new List<Dependency>()
