ac9d459df7d3adb68d9f10cb83206aa6d4aa1be6
EdibSU <edib.supic@skyline.be>
2023-10-31 07:47:03 +0100

    SLProtocolProvider removed. Configuration now takes protocol as parameter.
    Change-Id: Ie7005a2ac59dda26462b55b75a9aa1fbe7d921d7

--- QAction_1/Client/PollingManagerConfiguration.cs
+++ QAction_1/Client/PollingManagerConfiguration.cs
@@ -3,9 +3,10 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    using Skyline.DataMiner.Scripting;
+
     using Skyline.PollingManager.Enums;
     using Skyline.PollingManager.Pollable;
-    using Skyline.PollingManager.Providers;
     using Skyline.PollingManager.Structs;
 
     public class PollingManagerConfiguration
@@ -14,43 +15,43 @@
 		{
 			// Parent of CEO, CFO, CTO
 			// Child of -
-			{ "Owner", new PollableA(SLProtocolProvider.Protocol, "Owner - A") },
+			{ "Owner", new PollableA(Protocol, "Owner - A") },
 
 			// Parent of CFO, CTO, Expert Hub Lead
 			// Child of Owner
-			{ "CEO", new PollableA(SLProtocolProvider.Protocol, "CEO - A") },
+			{ "CEO", new PollableA(Protocol, "CEO - A") },
 
 			// Parent of -
 			// Child of Owner, CEO
-			{ "CFO", new PollableB(SLProtocolProvider.Protocol, "CFO - B", 15) },
+			{ "CFO", new PollableB(Protocol, "CFO - B", 15) },
 
 			// Parent of Expert Hub Lead
 			// Child of Owner, CEO
-			{ "CTO", new PollableB(SLProtocolProvider.Protocol, "CTO - B", 20) },
+			{ "CTO", new PollableB(Protocol, "CTO - B", 20) },
 
 			// Parent of Principal 1, Principal 2, Senior 1
 			// Child of CEO, CTO
-			{ "Expert Hub Lead", new PollableB(SLProtocolProvider.Protocol, "Expert Hub Lead - B") },
+			{ "Expert Hub Lead", new PollableB(Protocol, "Expert Hub Lead - B") },
 
 			// Parent of Senior 2, Senior 3
 			// Child of CTO, Expert Hub Lead
-			{ "Principal 1", new PollableB(SLProtocolProvider.Protocol, "Principal 1 - B") },
+			{ "Principal 1", new PollableB(Protocol, "Principal 1 - B") },
 
 			// Parent of Senior 1
 			// Child of CTO, Expert Hub Lead
-			{ "Principal 2", new PollableC(SLProtocolProvider.Protocol, "Principal 2 - C", 30, 60, PeriodType.Default) },
+			{ "Principal 2", new PollableC(Protocol, "Principal 2 - C", 30, 60, PeriodType.Default) },
 
 			// Parent of -
 			// Child of Expert Hub Lead, Principal 2
-			{ "Senior 1", new PollableA(SLProtocolProvider.Protocol, "Senior 1 - A") },
+			{ "Senior 1", new PollableA(Protocol, "Senior 1 - A") },
 
 			// Parent of -
 			// Child of Principal 1
-			{ "Senior 2", new PollableB(SLProtocolProvider.Protocol, "Senior 2 - B") },
+			{ "Senior 2", new PollableB(Protocol, "Senior 2 - B") },
 
 			// Parent of -
 			// Child of Principal 1
-			{ "Senior 3", new PollableC(SLProtocolProvider.Protocol, "Senior 3 - C") },
+			{ "Senior 3", new PollableC(Protocol, "Senior 3 - C") },
 		};
 
 		private readonly List<Dependency> _dependencies = new List<Dependency>()
@@ -60,11 +61,14 @@
 			new Dependency("Working", true, "Must Equal Working is not working!"),
 		};
 
-		public PollingManagerConfiguration()
+		public PollingManagerConfiguration(SLProtocol protocol)
 		{
+			Protocol = protocol;
 			SetRelations();
 			SetDependencies();
 		}
+
+		public static SLProtocol Protocol { get; set; }
 
 		public List<PollableBase> Rows => _rows.Select(row => row.Value).ToList();
 
--- QAction_1/Providers/SLProtocolProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Skyline.PollingManager.Providers
-{
-	using Skyline.DataMiner.Scripting;
-
-	public class SLProtocolProvider
-	{
-		public static SLProtocol Protocol { get; set; }
-	}
-}
